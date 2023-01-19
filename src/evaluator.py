2bafd7b08b52d6c30edb14f36656bfbd567ada59
Jayden <jayden.hou12@gmail.com>
2023-01-19 16:00:42 +1300

    retrieve player data for each member

--- src/evaluator.py
+++ src/evaluator.py
@@ -6,9 +6,18 @@ headers = {
     'authorization': config.auth_key
 }
 
+
+def get_user(player_tag):
+    player = player_tag.replace('#', '%23')
+    response = requests.get('https://api.clashofclans.com/v1/players/'+player, headers = headers)
+    user_json = response.json() # convert object to json
+    return user_json['name']
+
+
+
 def get_clan():
     response = requests.get('https://api.clashofclans.com/v1/clans/%2329R2GLL89', headers = headers)
     for member in response.json()['memberList']:
-        print(member['name'])
+        print(get_user(member['tag']))
 
 get_clan()
